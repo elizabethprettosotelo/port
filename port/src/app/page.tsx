8e6c302aa6bd73a45e2607aa42ab99cfcb08a27f
Elizabeth Pretto-Sotelo ⊹.✮₊⋆ <elizabethprettosotelo@gmail.com>
2025-06-11 10:34:55 -0400

    ermm

--- port/src/app/page.tsx
+++ port/src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import BackgroundGradientAnimation from "@/components/background-gradient-animation";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <BackgroundGradientAnimation className="absolute inset-0 -z-10" />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
